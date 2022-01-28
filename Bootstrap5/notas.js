849d743686cc27e4f2250b64414824f313941a1d
Camila Budke <camila.budke@gmail.com>
2022-01-28 08:46:47 -0400

    27-01

---
+++ Bootstrap5/notas.js
@@ -0,0 +1,3 @@
+// Lançamento em 2021
+// Podemos incluí-lo através do CDN [Content Delivery Network], baixar o arquivo fonte pelo site [getbootstrap.com], e hospedá-lo no seu computador, ou através de um gerenciador de pacotes [npm].
+// Incluindo pelo CDN, basta incluir o link do CSS no <head> do documento e o link do JS no final do documento após o </body>. O JS do Bootstrap utiliza seus próprios plugins e o Popper [https://popper.js.org/docs/v2/]
