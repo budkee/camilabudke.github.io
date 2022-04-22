b3413d3e0417c66d992cfd9430247c6dbe9a15d0
Camila Budke <74264225+budkee@users.noreply.github.com>
2022-04-22 13:19:22 -0400

    Configurando e recapitulando python

---
+++ Python/Curso em Vídeo/mundo 01/desafio10.py
@@ -0,0 +1,8 @@
+cart = float(input('Quanto reais você tem? '))
+cot = float(input('Qual é a cotação do dólar hoje? '))
+
+valor = cart / cot
+
+print('Você tem {:4f} dólares na carteira.'.format(valor)) 
+
+# Revisar a formatação.
---
+++ Python/Curso em Vídeo/mundo 01/desafio12.py
@@ -0,0 +1,5 @@
+preco = float(input('Valor do produto: '))
+desc = 5/100 * preco
+preco_desc = preco - desc
+
+print('Preço com um desconto de 5% fica R${}'.format(preco_desc))
