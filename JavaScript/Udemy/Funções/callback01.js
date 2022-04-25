08b8f16271b76d3306550b2b89f2e0a6f4764ffa
Camila Budke <74264225+budkee@users.noreply.github.com>
2022-04-25 10:43:12 -0400

    Término da secção teórica de Fundamentos de JS
    Próximo tópico: Resolução de Exercícios (Udemy)

--- JavaScript/Udemy/Funções/callback01.js
+++ JavaScript/Udemy/Funções/callback01.js
@@ -4,6 +4,7 @@ function imprimir(nome, indice) {
     console.log(`${indice + 1} . ${nome}`)
 }
 console.log('Lista dos fabricantes:')
-fabricantes.forEach(imprimir) // A função .forEach vá primeiros pegar o parâmetro valor, depois o parâmetro índice. (Acredito que seja por padrão.) 
+fabricantes.forEach(imprimir) // A função .forEach vai primeiro pegar o parâmetro valor, depois o parâmetro índice. 
+
 console.log('\nFabricantes:')
 fabricantes.forEach(fabricante => console.log(fabricante))
